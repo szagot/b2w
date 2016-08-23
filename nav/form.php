a6c9cc4081de11d514d6158b7878e7b65f9d1b4b
Kamile Pimenta <kamile@tmw.com.br>
2016-08-23 16:31:48 -0300

    Acrescentado Labels

--- nav/form.php
+++ nav/form.php
@@ -12,7 +12,9 @@ if (! isset($descTypes)) {
     </head>
     <body>
         <form id="formB2W" action="" method="post">
+            <label class="title">ID Parceiro</label>
             <input class="text" type="text" name="id_parceiro" id="id_parceiero" value="<?= $idParceiro ?>">
+            <label class="title">Descrição</label>
             <select class="desc" name="desc_type" id="desc_type">
                 <option value="curta" <?= ($descType == 'curta') ? 'selected' : '' ?>>Descrição Curta</option>
                 <option value="default" <?= ($descType == 'default' || empty($descType)) ? 'selected' : '' ?>>Descrição Padrão</option>
