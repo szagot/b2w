72e361a691bbf4bb7afb818f94587641b70ff156
Daniel Bispo <daniel@tmw.com.br>
2016-08-23 16:26:37 -0300

    Testando padrões

--- gen.php
+++ gen.php
@@ -7,24 +7,45 @@
  */
 require_once '../config/conecta.class.php';
 
+// Inicializa variáveis
 $idParceiro = filter_input(INPUT_POST, 'id_parceiro');
 $descType = filter_input(INPUT_POST, 'desc_type');
+$pathConf = __DIR__ . DIRECTORY_SEPARATOR . 'b2w.conf';
 $pdo = new Conecta();
 
 // ID Parceiro não informado?
 if (! $idParceiro) {
     header('Content-type: text/html; charset=utf-8');
 
+    // Verificando arquivo de pesquisa
+    if (file_exists($pathConf)) {
+        $config = json_decode(file_get_contents($pathConf));
+        // Verificando existencia de chaves
+        if (isset($config->idParceiro, $config->descType)) {
+            $idParceiro = $config->idParceiro;
+            $descType = $config->descType;
+        }
+    }
+
     // Pegando Descrições disponíveis
-    $descicoes = $pdo->execute('SELECT DES_NOME FROM descricao_prod GROUP BY DES_NOME ORDER BY DES_NOME');
+    $descricoes = $pdo->execute('SELECT DES_NOME FROM descricao_prod GROUP BY DES_NOME ORDER BY DES_NOME');
     $descTypes = [];
-    foreach ($descicoes as $desc) {
-        $descTypes[] = $desc->DES_NOME;
+    if ($descricoes) {
+        foreach ($descricoes as $desc) {
+            $descTypes[] = $desc->DES_NOME;
+        }
     }
 
     // Montando form
     require_once 'nav/form.php';
     exit;
 }
+
+// Gravando Valores escolhidos
+file_put_contents($pathConf, json_encode([
+    'idParceiro' => $idParceiro,
+    'descType'   => $descType
+]));
+
 
 // Gerando pesquisa
