91c3bede5fa3c0c8b7b804336cf6756b7cd21bba
Kamile Pimenta <kamile@tmw.com.br>
2016-08-23 16:31:48 -0300

    Acrescentado Labels

--- nav/form.php
+++ nav/form.php
@@ -12,19 +12,27 @@ if (! isset($descTypes)) {
     </head>
     <body>
         <form id="formB2W" action="" method="post">
-            <input class="text" type="text" name="id_parceiro" id="id_parceiero" value="<?= $idParceiro ?>">
-            <select class="desc" name="desc_type" id="desc_type">
-                <option value="curta" <?= ($descType == 'curta') ? 'selected' : '' ?>>Descrição Curta</option>
-                <option value="default" <?= ($descType == 'default' || empty($descType)) ? 'selected' : '' ?>>Descrição Padrão</option>
-                <?php
-                foreach ($descTypes as $desc) {
-                    ?>
-                    <option <?= ($descType == $desc) ? 'selected' : '' ?>><?= $desc ?></option>
+            <div class="campo">
+                <label class="title">ID Parceiro</label>
+                <input class="text" type="text" name="id_parceiro" id="id_parceiero" value="<?= $idParceiro ?>">
+            </div>
+            <div class="campo">
+                <label class="title">Descrição</label>
+                <select class="desc" name="desc_type" id="desc_type">
+                    <option value="curta" <?= ($descType == 'curta') ? 'selected' : '' ?>>Descrição Curta</option>
+                    <option value="default" <?= ($descType == 'default' || empty($descType)) ? 'selected' : '' ?>>Descrição Padrão</option>
                     <?php
-                }
-                ?>
-            </select>
-            <button class="btn" type="submit">Gerar</button>
+                    foreach ($descTypes as $desc) {
+                        ?>
+                        <option <?= ($descType == $desc) ? 'selected' : '' ?>><?= $desc ?></option>
+                        <?php
+                    }
+                    ?>
+                </select>
+            </div>
+            <div class="campo">
+                <button class="btn" type="submit">Gerar</button>
+            </div>
         </form>
     </body>
 </html>
