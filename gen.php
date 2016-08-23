c1fecaed2a7e6752d9524a03f943a3f09d0afc9d
Daniel Bispo <daniel@tmw.com.br>
2016-08-23 15:51:27 -0300

    Protegido arquivo de formulario e concluido chamada do mesmo

--- gen.php
+++ gen.php
@@ -5,13 +5,26 @@
  * Date: 23/08/2016
  * Time: 15:05
  */
+require_once '../config/conecta.class.php';
 
 $idParceiro = filter_input(INPUT_POST, 'id_parceiro');
 $descType = filter_input(INPUT_POST, 'desc_type');
+$pdo = new Conecta();
 
-if( ! $idParceiro ){
+// ID Parceiro não informado?
+if (! $idParceiro) {
+    header('Content-type: text/html; charset=utf-8');
+
+    // Pegando Descrições disponíveis
+    $descicoes = $pdo->execute('SELECT DES_NOME FROM descricao_prod GROUP BY DES_NOME ORDER BY DES_NOME');
+    $descTypes = [];
+    foreach ($descicoes as $desc) {
+        $descTypes[] = $desc->DES_NOME;
+    }
+
+    // Montando form
     require_once 'nav/form.php';
     exit;
 }
 
-require_once '../config/conecta.class.php';
+// Gerando pesquisa
--- nav/form.php
+++ nav/form.php
@@ -1,3 +1,8 @@
+<?php
+if (! isset($descTypes)) {
+    exit;
+}
+?>
 <!DOCTYPE html>
 <html lang="pt-br">
     <head>
@@ -6,20 +11,20 @@
         <link href="estilo.css" rel="stylesheet" type="text/css">
     </head>
     <body>
-    <form id="formB2W" action="" method="post">
-        <input class="text" type="text" name="id_parceiro" id="id_parceiero">
-        <select class="desc" name="desc_type" id="desc_type">
-            <option value="curta">Descrição Curta</option>
-            <option value="default" selected>Descrição Padrão</option>
-            <?php
-                foreach( $descTypes as $descType ){
+        <form id="formB2W" action="" method="post">
+            <input class="text" type="text" name="id_parceiro" id="id_parceiero">
+            <select class="desc" name="desc_type" id="desc_type">
+                <option value="curta">Descrição Curta</option>
+                <option value="default" selected>Descrição Padrão</option>
+                <?php
+                foreach ($descTypes as $descType) {
                     ?>
                     <option><?= $descType ?></option>
                     <?php
                 }
-            ?>
-        </select>
-        <button class="btn" type="submit">Gerar</button>
-    </form>
+                ?>
+            </select>
+            <button class="btn" type="submit">Gerar</button>
+        </form>
     </body>
 </html>
