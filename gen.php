606af347d91c906196a45c41c355cbed8416fbca
= <=>
2016-08-23 15:13:52 -0300

    Iniciado projeto

--- gen.php
+++ gen.php
@@ -6,3 +6,12 @@
  * Time: 15:05
  */
 
+$idParceiro = filter_input(INPUT_POST, 'id_parceiro');
+$descType = filter_input(INPUT_POST, 'desc_type');
+
+if( ! $idParceiro ){
+    require_once 'nav/form.html';
+    exit;
+}
+
+require_once '../config/conecta.class.php';
